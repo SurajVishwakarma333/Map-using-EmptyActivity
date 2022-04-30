4110c865cef36f5b303bceaaa545c64cb038733c
Http Request App <vishwakarmasuraj678@gmail.com>
2022-04-30 11:14:58 +0530

    map

---
+++ app/src/main/java/com/example/chaseapppractice/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.chaseapppractice;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.Map;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        LatLng Mumbai = new LatLng(19.17, 72.83);
+        map=googleMap;
+
+        map.addMarker(new MarkerOptions().position(Mumbai).title("Marker in India"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(Mumbai));
+    }
+}
